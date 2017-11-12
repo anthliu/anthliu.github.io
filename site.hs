3552c171f4a8a838390b5cf1f45ece47db4b7d51
Anthony Liu <liu916@gmail.com>
2017-11-12 18:22:33 -0500

    add stuff

--- site.hs
+++ site.hs
@@ -30,29 +30,11 @@ main = config >>= \cfg -> hakyllWith cfg $ do
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
 
-    create ["archive.html"] $ do
-        route idRoute
-        compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
-
-            makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
-
-
     match "index.html" $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
             let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
-                    defaultContext
+                    constField "title" "Anthony Liu" `mappend` defaultContext
 
             getResourceBody
                 >>= applyAsTemplate indexCtx
